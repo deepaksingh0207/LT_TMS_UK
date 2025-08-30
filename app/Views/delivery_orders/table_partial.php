90a50bb4dba44fa19ebfe87e93378df0af8ab98b
HerambGite-FT <herambg@fts-pl.com>
2025-08-30 18:03:00 +0530

    started with transporter assignment to delivery orders

--- app/Views/delivery_orders/table_partial.php
+++ app/Views/delivery_orders/table_partial.php
@@ -1,6 +1,15 @@
 <div class="col-xl-12 col-lg-12 col-md-12 card-box pb-10">
     <div class="row h5 pd-20 mb-0">
-        <div class="col-md-10">Delivery Orders</div>
+        <div class="col-md-4">Delivery Orders</div>
+        <div class="col-md-4">
+            <div class="from-group">
+                <div class="dropdown bootstrap-select form-control">
+                    <select name="" id="" class="selectpicker form-control" data-style=btn-outline-primary>
+                        <option value="">---Select Transporter---</option>
+                    </select>
+                </div>
+            </div>
+        </div>
         <div class="col-md-2 text-right">
 
         </div>
@@ -18,7 +27,16 @@
             <?php if (!empty($orders)): ?>
                 <?php foreach ($orders as $order): ?>
                     <tr>
-                        <td><?php echo $order['order_no']; ?></td>
+                        <td>
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox mb-5">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck_<?php echo $order['id']; ?>">
+                                    <label class="custom-control-label" for="customCheck_<?php echo $order['id']; ?>">
+                                        <?php echo $order['order_no']; ?>
+                                    </label>                                    
+                                </div>                               
+                            </div>
+                        </td>
                         <td><?php echo $order['delivery_date']; ?></td>
                         <td><?php echo "Code - ".$order['ship_to_party_code']."<br> Name - ".$order['ship_to_party_name']; ?></td>
                         <td><?php echo "Code - ".$order['sold_to_party_code']."<br> Name - ".$order['sold_to_party_name']; ?></td>
